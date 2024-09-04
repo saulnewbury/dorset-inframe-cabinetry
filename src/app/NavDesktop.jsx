e091bfbf8ec32bff4a3ee37888f6f208c5c091a0
saulnewbury <saulnewbury@gmail.com>
2024-09-04 18:43:41 +0100

    Breadcrumb trail

--- src/app/NavDesktop.jsx
+++ src/app/NavDesktop.jsx
@@ -16,73 +16,88 @@ export default function NavDesktop() {
   const pathname = usePathname()
 
   useEffect(() => {
+    console.log(pathname.split('/').slice(1).length > 1)
     setIsOpen(false)
   }, [pathname])
 
   return (
-    <nav className='hidden lg:flex bg-white fixed w-[100vw] px-[37px] h-[4.5rem] justify-between items-center font-medium text-[14px] border-b border-lightGrey border-solid'>
-      <div className='h-[max-content]'>
-        <Link href='/'>
-          <span className='font-medium cursor-pointer'>
-            Dorset Inframe Cabinetry
+    <>
+      <nav className='hidden lg:flex bg-white fixed w-[100vw] px-[37px] h-[4.5rem] justify-between items-center font-medium text-[14px] border-b border-lightGrey border-solid'>
+        <div className='h-[max-content]'>
+          <Link href='/'>
+            <span className='font-medium cursor-pointer'>
+              Dorset Inframe Cabinetry
+            </span>
+          </Link>
+        </div>
+
+        {isOpen && (
+          <div className='backpanel bg-white h-[360px] w-full fixed top-[72px] left-0'></div>
+        )}
+
+        <div className='links h-full flex items-center'>
+          {menuDesk.map((item, i) => {
+            return item.submenu ? (
+              <div
+                key={i}
+                className='link-container h-full'
+                onMouseEnter={() => setIsOpen(item.name)}
+                onMouseLeave={() => setIsOpen(false)}
+              >
+                <Link
+                  className={`link ${
+                    pathname.startsWith(item.url) ? 'active' : ''
+                  } cursor-pointer mr-[36px] h-full flex items-center relative`}
+                  href={item.url}
+                >
+                  <span>{item.name}</span>
+                </Link>
+                <SubmenuDesk
+                  className='dropdown'
+                  items={item.submenu}
+                  isOpen={item.name === isOpen} // compares instance url with state url
+                  closeMenu={() => setIsOpen(false)}
+                />
+              </div>
+            ) : (
+              <div key={i} className='link-container h-full'>
+                <Link
+                  className={`link ${
+                    pathname.startsWith(item.url) ? 'active' : ''
+                  } cursor-pointer mr-[36px] h-full flex items-center`}
+                  href={item.url}
+                >
+                  <span>{item.name}</span>
+                </Link>
+              </div>
+            )
+          })}
+
+          <span className='inline-block relative cursor-pointer'>
+            <SvgIcon shape='list' />
+            <div className='w-[0.9rem] h-[0.9rem] bg-[black] rounded-full absolute bottom-[4px] -right-[7px] flex justify-center items-center'>
+              <span className='text-[#ffffff] text-[0.5rem] font-bold'>25</span>
+            </div>
           </span>
-        </Link>
-      </div>
 
-      {isOpen && (
-        <div className='backpanel bg-white h-[360px] w-full fixed top-[72px] left-0'></div>
-      )}
-
-      <div className='links h-full flex items-center'>
-        {menuDesk.map((item, i) => {
-          return item.submenu ? (
-            <div
-              key={i}
-              className='link-container h-full'
-              onMouseEnter={() => setIsOpen(item.name)}
-              onMouseLeave={() => setIsOpen(false)}
-            >
-              <Link
-                className={`link ${
-                  pathname.startsWith(item.url) ? 'active' : ''
-                } cursor-pointer mr-[36px] h-full flex items-center relative`}
-                href={item.url}
-              >
-                <span>{item.name}</span>
-              </Link>
-              <SubmenuDesk
-                className='dropdown'
-                items={item.submenu}
-                isOpen={item.name === isOpen} // compares instance url with state url
-                closeMenu={() => setIsOpen(false)}
-              />
-            </div>
-          ) : (
-            <div key={i} className='link-container h-full'>
-              <Link
-                className={`link ${
-                  pathname.startsWith(item.url) ? 'active' : ''
-                } cursor-pointer mr-[36px] h-full flex items-center`}
-                href={item.url}
-              >
-                <span>{item.name}</span>
-              </Link>
-            </div>
-          )
-        })}
-
-        <span className='inline-block relative cursor-pointer'>
-          <SvgIcon shape='list' />
-          <div className='w-[0.9rem] h-[0.9rem] bg-[black] rounded-full absolute bottom-[4px] -right-[7px] flex justify-center items-center'>
-            <span className='text-[#ffffff] text-[0.5rem] font-bold'>25</span>
-          </div>
-        </span>
-
-        {/* <span className='inline-block cursor-pointer'>
+          {/* <span className='inline-block cursor-pointer'>
           <span className='mr-[5px]'>Login</span>
           <SvgIcon shape='login' />
         </span> */}
-      </div>
-    </nav>
+        </div>
+      </nav>
+      {pathname.split('/').slice(1).length > 1 && (
+        <div className='fixed font-medium text-[14px] top-[4.5rem] py-2 pl-[20px] sm:pl-[37px]'>
+          {[pathname.split('/')].shift().map((crumb, i, arr) => {
+            const text = crumb.charAt(0).toUpperCase() + crumb.slice(1)
+            if (i > 0 && i < arr.length - 1) {
+              return <span>{text}&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+            } else {
+              return <span>{text}</span>
+            }
+          })}
+        </div>
+      )}
+    </>
   )
 }
