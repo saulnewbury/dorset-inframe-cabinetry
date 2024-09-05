20eb6f40d4f8e5ebdee4bff588185a7d00a39239
saulnewbury <saulnewbury@gmail.com>
2024-09-05 09:52:17 +0100

    Save

--- src/app/lib/components/HeroText.jsx
+++ src/app/lib/components/HeroText.jsx
@@ -6,7 +6,7 @@ export default function HeroText({ markup }) {
   return (
     <section className='h-[400px] flex items-center px-[20px] sm:px-[37px]'>
       <div
-        className='mt-[80px] md:px-[5vw] text-[28px] leading-[34px] font-normal'
+        className='mt-[80px] md:px-[5vw] text-[24px] lg:text-[28px] leading-[32px] lg:leading-[37px] font-normal'
         dangerouslySetInnerHTML={createMarkup(markup)}
       />
     </section>
--- src/app/lib/data/data.js
+++ src/app/lib/data/data.js
@@ -1,7 +1,2 @@
 export const homeHeroMarkup =
-  '<p class="max-w-[706px]">We pride ourselves on delivering affordable high-end, inframe cabinetry, made to last, using materials that are both cost effective and eco-friendly.</p>'
-
-export const heroMaterialsMarkup = '<p>Materials</p>'
-
-export const heroMdfMarkup =
-  '<p>We pride ourselves on delivering affordable high- <br />end, inframe cabinetry, made to last, using materials <br />that are both cost effective and eco-friendly.</p>'
+  '<p class="max-w-[600px] lg:max-w-[706px]">We pride ourselves on delivering affordable high-end, inframe cabinetry, made to last, using materials that are both cost effective and eco-friendly.</p>'
