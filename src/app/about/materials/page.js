9305aec3921fb2319b5924c4645bf26ed5e25283
saulnewbury <saulnewbury@gmail.com>
2024-09-05 10:04:05 +0100

    Save

--- src/app/about/materials/page.js
+++ src/app/about/materials/page.js
@@ -1,14 +1,12 @@
 'use client'
 import HeroText from '@/lib/components/HeroText'
 import HeroImage from '@/lib/components/HeroImage'
-
-import { heroMaterialsMarkup } from '@/lib/data/data'
 import materials from '@/lib/images/materials.jpg'
 
 export default function page() {
   return (
     <>
-      <HeroText markup={heroMaterialsMarkup} />
+      <HeroText markup='Materials' />
       <HeroImage src={materials} />
     </>
   )
