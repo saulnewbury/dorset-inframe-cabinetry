6f81b2b16c3c1dcde319edc941ad962014ed0def
saulnewbury <saulnewbury@gmail.com>
2025-04-04 13:33:09 +0100

    Updated door handle

--- src/app/(planner)/kitchen-planner/openings/DoorHandle.jsx
+++ src/app/(planner)/kitchen-planner/openings/DoorHandle.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { Edges, Outlines } from '@react-three/drei'
 
 export default function DoorHandle({ len, width, depth, orientation }) {
-  const handleShaft = 0.22
-  const handleRad = 0.015
-  const insertDepth = 0.05
-  const handleDepth = handleShaft + handleRad - insertDepth
+  const leverLength = 0.18
+  const shaftLength = 0.05
+  const rad = 0.015
 
   return (
     <>
       <group
         position-y={len}
         position-x={orientation === 'left' ? width / 4 - 0.1 : -width / 4 + 0.1}
-        position-z={depth / 2 + handleDepth / 4 - 0.05}
+        position-z={depth / 2}
         rotation-x={-Math.PI * 0.5}
         rotation-y={orientation === 'left' ? Math.PI : Math.PI * 2}
       >
@@ -20,23 +19,15 @@ export default function DoorHandle({ len, width, depth, orientation }) {
         <mesh position-y={0.05}>
           <cylinderGeometry args={[0.05, 0.05, 0.0001]} />
           <meshStandardMaterial color='lightgrey' />
-          <Edges linewidth={2} threshold={15} color={'gray'} />
-          <Outlines thickness={0.01} color={'gray'} />
-        </mesh>
-        {/* Lever */}
-        <mesh>
-          <cylinderGeometry args={[handleRad, handleRad, 0.1]} />
-          <meshStandardMaterial color='lightgrey' />
-          <Edges linewidth={2} threshold={15} color={'gray'} />
-          <Outlines thickness={0.01} color={'gray'} />
         </mesh>
         {/* Shaft */}
-        <mesh
-          position-y={-insertDepth}
-          position-x={0.06}
-          rotation-z={Math.PI / 2}
-        >
-          <cylinderGeometry args={[handleRad, handleRad, handleShaft]} />
+        <mesh position-y={shaftLength / 2}>
+          <cylinderGeometry args={[rad, rad, shaftLength]} />
+          <meshStandardMaterial color='lightgrey' />
+        </mesh>
+        {/* Lever */}
+        <mesh position-x={0.06} rotation-z={Math.PI / 2}>
+          <cylinderGeometry args={[rad, rad, leverLength]} />
           <meshStandardMaterial color='lightgrey' />
           <Edges linewidth={1} threshold={15} color={'gray'} />
         </mesh>
@@ -45,7 +36,7 @@ export default function DoorHandle({ len, width, depth, orientation }) {
       <group
         position-y={len}
         position-x={orientation === 'left' ? width / 4 - 0.1 : -width / 4 + 0.1}
-        position-z={-depth / 2 - handleDepth / 4 + 0.05}
+        position-z={-depth / 2}
         rotation-x={Math.PI * 0.5}
         rotation-y={orientation === 'left' ? Math.PI : Math.PI * 2}
       >
@@ -53,22 +44,15 @@ export default function DoorHandle({ len, width, depth, orientation }) {
         <mesh position-y={0.05}>
           <cylinderGeometry args={[0.05, 0.05, 0.0001]} />
           <meshStandardMaterial color='lightgrey' />
-          <Edges linewidth={1} threshold={15} color={'gray'} />
-        </mesh>
-        {/* Lever */}
-        <mesh>
-          <cylinderGeometry args={[handleRad, handleRad, 0.1]} />
-          <meshStandardMaterial color='lightgrey' />
-          <Edges linewidth={1} threshold={15} color={'gray'} />
         </mesh>
         {/* Shaft */}
-        <mesh
-          position-y={-insertDepth}
-          // position-x={0.06}
-          position-x={0.06}
-          rotation-z={Math.PI / 2}
-        >
-          <cylinderGeometry args={[handleRad, handleRad, handleShaft]} />
+        <mesh position-y={shaftLength / 2}>
+          <cylinderGeometry args={[rad, rad, shaftLength]} />
+          <meshStandardMaterial color='lightgrey' />
+        </mesh>
+        {/* Lever */}
+        <mesh position-x={0.06} rotation-z={Math.PI / 2}>
+          <cylinderGeometry args={[rad, rad, leverLength]} />
           <meshStandardMaterial color='lightgrey' />
           <Edges linewidth={1} threshold={15} color={'gray'} />
         </mesh>
