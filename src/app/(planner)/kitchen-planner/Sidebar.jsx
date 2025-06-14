dd1f19476e6d090eca263290e832bba2ce6718d5
stevewlrls <stevewlrls@gmail.com>
2025-06-14 15:43:59 +0100

    Fix max-contnet -> max-content

--- src/app/(planner)/kitchen-planner/Sidebar.jsx
+++ src/app/(planner)/kitchen-planner/Sidebar.jsx
@@ -12,11 +12,11 @@ export default function Sidebar({
   showContent
 }) {
   return (
-    <div className='text-[12px] gutter fixed h-[max-contnet] w-[max-content] z-[200] top-[120px] left-0 flex flex-col justify-between'>
+    <div className="text-[12px] gutter fixed h-[max-content] w-[max-content] z-[200] top-[120px] left-0 flex flex-col justify-between">
       {menu.map((item, i) => (
         <Fragment key={i}>
           <button
-            type='button'
+            type="button"
             className={`${
               showContent ? 'pointer-events-none' : ''
             } relative h-[4rem] mb-[1rem] flex flex-col items-center hover:font-medium`}
@@ -28,33 +28,33 @@ export default function Sidebar({
             }}
           >
             <SvgIcon shape={item.name} />
-            <span className='mt-[.5rem]'>{toTitleCase(item.name)}</span>
+            <span className="mt-[.5rem]">{toTitleCase(item.name)}</span>
 
             {/* Triangle for menu box */}
             {open === item.name && (
-              <div className='h-[16px] w-[16px] mt-[10px] border-[8px] border-transparent border-r-[#F0F0EE] absolute left-[45px]' />
+              <div className="h-[16px] w-[16px] mt-[10px] border-[8px] border-transparent border-r-[#F0F0EE] absolute left-[45px]" />
             )}
             {/* Triangle for content box */}
             {showContent === item.name && (
-              <div className='h-[16px] w-[16px] mt-[10px] border-[8px] border-transparent border-r-white absolute left-[45px]' />
+              <div className="h-[16px] w-[16px] mt-[10px] border-[8px] border-transparent border-r-white absolute left-[45px]" />
             )}
 
             {/* Flyout */}
             {open === item.name && (
-              <div className='absolute left-[51px] -top-[10.5px] min-w-[7rem]'>
-                <div className='bg-blueTint py-[16px] px-[20px] w-full ml-[10px] leading-6 text-left'>
-                  <div className='flex gap-4'>
+              <div className="absolute left-[51px] -top-[10.5px] min-w-[7rem]">
+                <div className="bg-blueTint py-[16px] px-[20px] w-full ml-[10px] leading-6 text-left">
+                  <div className="flex gap-4">
                     {/* Items with variants displayed in a row */}
                     {item.options
                       .filter((option) => Object.hasOwn(option, 'variants'))
                       .map((option, idx) => (
-                        <div key={idx} className='group break-inside-avoid'>
-                          <h4 className='font-bold'>{option.name}</h4>
+                        <div key={idx} className="group break-inside-avoid">
+                          <h4 className="font-bold">{option.name}</h4>
                           <ul>
                             {option.variants.map((variant) => (
                               <li
                                 key={variant}
-                                className='hover:text-[gray] text-nowrap'
+                                className="hover:text-[gray] text-nowrap"
                                 onClick={() => {
                                   handleOpen('')
                                   handleShowContent(item.name)
@@ -73,13 +73,13 @@ export default function Sidebar({
                   {item.options.filter(
                     (option) => !Object.hasOwn(option, 'variants')
                   ).length > 0 && (
-                    <div className='flex flex-col'>
+                    <div className="flex flex-col">
                       {item.options
                         .filter((option) => !Object.hasOwn(option, 'variants'))
                         .map((option, idx) => (
                           <div
                             key={idx}
-                            className='text-nowrap hover:text-[gray]'
+                            className="text-nowrap hover:text-[gray]"
                             onClick={() => {
                               handleOpen('')
                               handleShowContent(item.name)
