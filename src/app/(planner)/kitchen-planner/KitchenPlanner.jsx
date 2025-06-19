6d01b44d623c6df1d46704e7a280d5cc08716a5e
stevelrls <stevewlrls@gmail.com>
2025-06-19 19:01:20 +0100

    Remove redundant path

--- src/app/(planner)/kitchen-planner/KitchenPlanner.jsx
+++ src/app/(planner)/kitchen-planner/KitchenPlanner.jsx
@@ -16,117 +16,108 @@ import { AppContext } from '@/context'
 import { useScreenshot } from '@/lib/helpers/useScreenshot'
 
 export default forwardRef(function KitchenPlanner(props, ref) {
-  const { is3D, set3D } = useAppState()
-  const [show, setShow] = useState(false)
-  const [shrink, setCanvasShrink] = useState(false)
-  const [refreshed, setRefreshed] = useState(false)
+	const { is3D, set3D } = useAppState()
+	const [show, setShow] = useState(false)
+	const [shrink, setCanvasShrink] = useState(false)
+	const [refreshed, setRefreshed] = useState(false)
 
-  const container = useRef()
-  const overlay = useRef()
+	const container = useRef()
+	const overlay = useRef()
 
-  // fade in animation for canvas when switching perspective
-  useGSAP(() => {
-    if (!refreshed) {
-      // Using an overlay to hide a glitchy flash
-      gsap.fromTo(
-        overlay.current,
-        { opacity: 1 },
-        { opacity: 0, duration: 0.8, delay: 1 }
-      )
-      gsap.set(overlay.current, {
-        visibility: 'hidden',
-        duration: 0.8,
-        delay: 1.8
-      })
-      setRefreshed(true)
-    }
-    if (is3D || !is3D || shrink || !shrink) {
-      gsap.fromTo(
-        container.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 0.8, delay: 0.04 }
-      )
-    } else {
-      console.log('Jack in the Box')
-      gsap.fromTo(
-        container.current,
-        { opacity: 0 },
-        { opacity: 1, duration: 0.01, delay: 0.04 }
-      )
-    }
-  }, [is3D, shrink])
+	// fade in animation for canvas when switching perspective
+	useGSAP(() => {
+		if (!refreshed) {
+			// Using an overlay to hide a glitchy flash
+			gsap.fromTo(
+				overlay.current,
+				{ opacity: 1 },
+				{ opacity: 0, duration: 0.8, delay: 1 }
+			)
+			gsap.set(overlay.current, {
+				visibility: 'hidden',
+				duration: 0.8,
+				delay: 1.8
+			})
+			setRefreshed(true)
+		}
+		gsap.fromTo(
+			container.current,
+			{ opacity: 0 },
+			{ opacity: 1, duration: 0.2, delay: 0.04 }
+		)
+	}, [is3D, shrink])
 
-  // show only on relevant pages
-  useImperativeHandle(
-    ref,
-    () => {
-      return {
-        showCanvas: () => {
-          setRefreshed(false)
-          setShow(true)
-        },
-        hideCanvas: () => {
-          setShow(false)
-        },
-        shrinkCanvas: () => {
-          setRefreshed(false)
-          setCanvasShrink(true)
-          setShow(true)
-        },
-        restoreCanvas: () => {
-          setRefreshed(false)
-          setCanvasShrink(false)
-          setShow(true)
-        }
-      }
-    },
-    []
-  )
+	// show only on relevant pages
+	useImperativeHandle(
+		ref,
+		() => {
+			return {
+				showCanvas: () => {
+					setRefreshed(false)
+					setShow(true)
+				},
+				hideCanvas: () => {
+					setShow(false)
+				},
+				shrinkCanvas: () => {
+					setRefreshed(false)
+					setCanvasShrink(true)
+					setShow(true)
+				},
+				restoreCanvas: () => {
+					setRefreshed(false)
+					setCanvasShrink(false)
+					setShow(true)
+				}
+			}
+		},
+		[]
+	)
 
-  const { takeScreenshot, SceneCapture } = useScreenshot(3840, 2160)
+	const { takeScreenshot, SceneCapture } = useScreenshot(3840, 2160)
 
-  const style = {
-    height: '70%',
-    width: '70%',
-    backgroundColor: '#ffffff',
-    marginTop: '6rem',
-    marginLeft: '37px',
-    border: 'solid',
-    borderColor: 'rgb(229, 231, 235)',
-    borderWidth: '1px'
-  }
+	const style = {
+		height: '70%',
+		width: '70%',
+		backgroundColor: '#ffffff',
+		marginTop: '6rem',
+		marginLeft: '37px',
+		border: 'solid',
+		borderColor: 'rgb(229, 231, 235)',
+		borderWidth: '1px'
+	}
 
-  return (
-    <AppContext.Provider value={is3D}>
-      <div
-        ref={container}
-        className={`canvas-container ${
-          show ? '' : 'opacity-0'
-        }  w-full h-full fixed`}
-      >
-        <Canvas frameloop="demand" shadows style={shrink ? style : ''}>
-          <Camera is3D={is3D} />
-          <Experience is3D={is3D} />
-          <SceneCapture />
-        </Canvas>
-        <div
-          ref={overlay}
-          className="absolute bg-[white] w-[100vw] h-[70vw] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
-        ></div>
-      </div>
-      <ViewControls
-        changePerspective={(bool) => set3D(bool)}
-        is3D={is3D}
-        shrink={shrink}
-      />
-      {shrink && (
-        <div
-          className="absolute top-[110px] left-[50px]"
-          onClick={takeScreenshot}
-        >
-          <SvgIcon classes="w-[35px] h-[35px]" shape="capture" />
-        </div>
-      )}
-    </AppContext.Provider>
-  )
+	return (
+		<AppContext.Provider value={is3D}>
+			<div
+				ref={container}
+				className={`canvas-container ${
+					show ? '' : 'opacity-0'
+				}  w-full h-full fixed`}
+			>
+				<Canvas frameloop='demand' shadows style={shrink ? style : ''}>
+					<Camera is3D={is3D} />
+					<Experience is3D={is3D} />
+					<SceneCapture />
+				</Canvas>
+				<div
+					ref={overlay}
+					className='absolute bg-[white] w-[100vw] h-[70vw] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'
+				></div>
+			</div>
+			<ViewControls
+				changePerspective={bool => set3D(bool)}
+				is3D={is3D}
+				shrink={shrink}
+			/>
+			{shrink && (
+				<div
+					className='absolute top-[110px] left-[50px]'
+					onClick={takeScreenshot}
+				>
+					<SvgIcon classes='w-[35px] h-[35px]' shape='capture' />
+				</div>
+			)}
+		</AppContext.Provider>
+	)
 })
