8777e83785ffbc94c7f0d54a0ce7fa592691f84c
stevelrls <stevewlrls@gmail.com>
2025-06-19 22:59:22 +0100

    Remove old method of verifying email

--- src/app/(planner)/kitchen-planner/layout.js
+++ src/app/(planner)/kitchen-planner/layout.js
@@ -1,56 +1,32 @@
 'use client'
-import { useRef, useState, useEffect, useContext, useMemo } from 'react'
-import { ModelContext } from '@/model/context'
-import { CanvasContext, SessionContext } from '@/context'
-import { useSearchParams } from 'next/navigation'
+import { useRef, useState, useEffect } from 'react'
+import { CanvasContext } from '@/context'
 
 // Components
 import KitchenPlanner from './KitchenPlanner'
 import NavConfigurator from './NavConfigurator'
 // import IntroMessage from './IntroMessage'
-import VerifyEmailDialog from './dialog/VerifyEmail'
 
 import PerspectiveContextProvider from './perspectiveContextProvider'
 
 export default function Layout({ children }) {
-  const [ref, setRef] = useState({})
-  const kitchenPlanner = useRef()
-  const [model] = useContext(ModelContext)
-  const search = useSearchParams()
-  const [verifyId] = useState(search.get('verifyId')) // retain from initial URL
-  const [session] = useContext(SessionContext)
+	const [ref, setRef] = useState({})
+	const kitchenPlanner = useRef()
 
-  // Remove query string from URL after user has verified their email address.
-  useEffect(() => {
-    if (verifyId && session) {
-      const url = new URL(window.location.href)
-      url.searchParams.delete('verifyId')
-      window.history.replaceState({}, document.title, url.toString())
-    }
-  }, [verifyId, session])
+	useEffect(() => {
+		setRef(kitchenPlanner) // give ref to CanvasContext
+	}, [])
 
-  useEffect(() => {
-    setRef(kitchenPlanner) // give ref to CanvasContext
-  }, [])
-
-  return (
-    <>
-      <PerspectiveContextProvider>
-        <NavConfigurator />
-        <CanvasContext.Provider value={ref}>
-          {/* <IntroMessage show={!verifyId} /> */}
-          <KitchenPlanner ref={kitchenPlanner} />
-          {children}
-        </CanvasContext.Provider>
-      </PerspectiveContextProvider>
-
-      <VerifyEmailDialog
-        verifyId={verifyId}
-        onVerify={(session) => {
-          doLogin(session)
-          setShowSubmitModel(!!model.id)
-        }}
-      />
-    </>
-  )
+	return (
+		<>
+			<PerspectiveContextProvider>
+				<NavConfigurator />
+				<CanvasContext.Provider value={ref}>
+					{/* <IntroMessage show={!verifyId} /> */}
+					<KitchenPlanner ref={kitchenPlanner} />
+					{children}
+				</CanvasContext.Provider>
+			</PerspectiveContextProvider>
+		</>
+	)
 }
